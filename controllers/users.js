fa60040f188872a32b8d4eec2f1467ac59e53e7e
davide <demartini.davide01@gmail.com>
2023-05-09 18:26:17 +0200

    creato update user e delete user

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 import User from '../models/user.js';
 import Group from '../models/group.js';
 import Outgoing from '../models/outgoing.js';
+import bcrypt from 'bcrypt';
 
 export const getUser = async (req, res) => {
     try {
@@ -35,9 +36,49 @@ export const getUserOutgoings = async (req, res) => {
 }
 
 export const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {
+            email,
+            nickname,
+            password,
+            firstName,
+            lastName,
+        } = req.body;
+        const user = await User.findById(id);
+        if(!user)
+            return res.status(404).json({ message: 'User not found' });
 
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        user.email = email;
+        user.nickname = nickname;
+        user.password = passwordHash;
+        user.firstName = firstName;
+        user.lastName = lastName;
+
+        try {
+            await user.save();
+        } catch (error) {
+            res.status(409).json({ message: error.message });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 export const deleteUser = async (req, res) => {
-
+    try {
+        const { id } = req.params;
+        try{
+            const user = await User.findByIdAndDelete(id);
+        } catch (error) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
