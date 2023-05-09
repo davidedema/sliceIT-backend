f50f258f9de01b7bdb3ad38b31b08b5405ba601b
davide <demartini.davide01@gmail.com>
2023-05-09 17:24:10 +0200

    funzione get groups e user

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 import User from '../models/user.js';
+import Group from '../models/group.js';
+import Outgoing from '../models/outgoing.js';
 
 export const getUser = async (req, res) => {
     try {
@@ -11,11 +13,25 @@ export const getUser = async (req, res) => {
 }
 
 export const getUserGroups = async (req, res) => {
-
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        const groups = await Group.find({ _id: { $in: user.groups } });
+        res.status(200).json(groups);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
 export const getUserOutgoings = async (req, res) => {
-
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        const outgoings = await Outgoing.find({ _id: { $in: user.outgoings } });
+        res.status(200).json(outgoings);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
 export const updateUser = async (req, res) => {
