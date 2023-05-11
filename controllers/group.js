cbd2f1d00988754ecba74f7df58bc2c0cc1ce10a
Vitho306 <francesco.poinelli@studenti.unitn.it>
2023-05-11 11:08:08 +0200

    sistemata identazione gruppi

--- controllers/group.js
+++ controllers/group.js
@@ -1,54 +1,51 @@
-import Group from '../models/group.js';
-import User from '../models/user.js';
+import Group from "../models/group.js";
+import User from "../models/user.js";
 
 /*FUNZIONE CHE GENERA UN LINK D'INVITO UNIVOCO*/
 function generateInviteLink() {
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let inviteLink = '';
-    for (let i = 0; i < 20; i++) {
-      inviteLink += characters[Math.floor(Math.random() * characters.length)];
-    }
-    return inviteLink;
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let inviteLink = "";
+  for (let i = 0; i < 20; i++) {
+    inviteLink += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return inviteLink;
 }
 
 /*CREARE UN GRUPPO*/
-export const createGroup = async (req, res) => { 
-    try {     
-        const { 
-            name,
-            descriprion,
-            groupPicture
-        } = req.body;   
-        const inviteLink = generateInviteLink();
-        const group = new Group({ name, descriprion, groupPicture, inviteLink });
-        await group.save(); 
-        res.status(201).json({ group });
-        } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: error.message });
-    }
+export const createGroup = async (req, res) => {
+  try {
+    const { name, descriprion, groupPicture } = req.body;
+    const inviteLink = generateInviteLink();
+    const group = new Group({ name, descriprion, groupPicture, inviteLink });
+    await group.save();
+    res.status(201).json({ group });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: error.message });
+  }
 };
 
 /*UNIRSI AD UN GRUPPO TRAMITE LINK INVITO*/
-export const joinGroup = async (req, res) => { 
-    try {
-      const { inviteLink } = req.query;
-      const user = JSON.parse(req.body.user);
-      
-      const group = await Group.findOne({ inviteLink });
-      if (!group) {
-        res.status(404).send('Group not found');
-        return;
-      }
-      if (group.members.includes(user._id)) {
-        res.status(400).send('User already a member of the group');
-          return;
-      }
-      group.members.push(user._id);
-      await group.save();
-      res.json({ group });
-      } catch (error) {
-          console.error(error);
-          res.status(500).send('Internal server error');
-      }
+export const joinGroup = async (req, res) => {
+  try {
+    const { inviteLink } = req.query;
+    const user = JSON.parse(req.body.user);
+
+    const group = await Group.findOne({ inviteLink });
+    if (!group) {
+      res.status(404).send("Group not found");
+      return;
+    }
+    if (group.members.includes(user._id)) {
+      res.status(400).send("User already a member of the group");
+      return;
+    }
+    group.members.push(user._id);
+    await group.save();
+    res.json({ group });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
 };
