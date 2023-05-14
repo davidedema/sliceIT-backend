b0719f5b4b87b7d207119bf96df10b8ebf0c4440
Vitho306 <francesco.poinelli@studenti.unitn.it>
2023-05-14 23:03:51 +0200

    first try of documentation

--- routes/group.js
+++ routes/group.js
@@ -18,6 +18,64 @@ const router = express.Router();
  *  description: API to handle Groups
  */
 
+/**
+ * @swagger
+ * /api/v1/group/createGroup:
+ *   post:
+ *     summary: create a new group
+ *     description: create a new group
+ *     tags:
+ *       -
+ *     produces:
+ *       -
+ *     consumes:
+ *       -
+ *     requestBody:
+ *       content:
+ *
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - inviteLink
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "name"
+ *                 description: Name of the group
+ *               inviteLink:
+ *                 type: string
+ *                 example: "2Yq4Z5Q7X1x3Y4z5Q7x"
+ *                 description: Link to join the group
+ *     responses:
+ *       201:
+ *         description: Group created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                group:
+ *                 type: object
+ *                 properties:
+ *                  name:
+ *                    type: string
+ *                    example: "name"
+ *                  inviteLink:
+ *                    type: string
+ *                    example: "2Yq4Z5Q7X1x3Y4z5Q7x"
+ *
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Internal server error"
+ */
 router.post("/createGroup", validateToken, createGroup);
 router.put("/joinGroup", validateToken, joinGroup);
 router.put("/updateGroup/:groupId", validateToken, updateGroup);
