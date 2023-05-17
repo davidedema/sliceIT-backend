4ac99efab2eef8ff4c0c98a3ab97cd5aca5cd699
davide <demartini.davide01@gmail.com>
2023-05-17 17:23:38 +0200

    deleted password from response

--- controllers/users.js
+++ controllers/users.js
@@ -9,6 +9,7 @@ export const getUser = async (req, res) => {
         const { id } = req.params;
         try {
             const user = await User.findById(id);
+            user.password = undefined;
             res.status(200).json(user);
         } catch (error) {
             return res.status(404).json({ message: 'User not found' });
