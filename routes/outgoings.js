289802be301d112f055ca433c5648bef640e1705
davide <demartini.davide01@gmail.com>
2023-05-23 10:50:01 +0200

    created route

--- routes/outgoings.js
+++ routes/outgoings.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
-
+    createOutgoing,
 } from "../controllers/outgoings.js";
 import { validateToken } from "../middleware/auth.js";
 
 const router = express.Router();
+
+router.post("/", validateToken, createOutgoing);
 
 export default router;
