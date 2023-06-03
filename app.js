658240c346c82b09354910295af09636fe787e6c
davide <demartini.davide01@gmail.com>
2023-06-03 12:51:31 +0200

    removed unnecessary

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 /* CONFIGURATIONS */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(helmet());
