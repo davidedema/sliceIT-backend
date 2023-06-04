bd5ce1f1e3db0fda04f6484b1f3011104bbc7cd7
davide <demartini.davide01@gmail.com>
2023-06-04 19:27:38 +0200

    deleted unnecessary config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-/*     collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageProvider: "v8", */
-    setupFiles: ["<rootDir>/.jest/setEnvVars.js"],
-    testEnvironment: "node",
-    verbose: true
-};
-
